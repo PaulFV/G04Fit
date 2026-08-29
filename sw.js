92a9ec3e32ca3a7cfe4f7add3125f5b4e8dbad0b
PaulFV <fodorpaul@web.de>
2026-08-30 00:34:37 +0200

    Fill exercise card preview with realistic demo

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    eigenen Programmdateien zwischengespeichert – keine
    Nutzerdaten, keine Anfragen an fremde Server.
    ============================================================ */
-var CACHE = 'gofit-v1.0.6';
+var CACHE = 'gofit-v1.0.7';
 
 var ASSETS = [
   './',
