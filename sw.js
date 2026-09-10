c5ea0987f1207533f984253b26d1150a522e5b83
PaulFV <155884041+PaulFV@users.noreply.github.com>
2026-09-10 07:50:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    eigenen Programmdateien zwischengespeichert. Der optionale
    Push-Dienst wird nur nach ausdrücklicher Einwilligung verwendet.
    ============================================================ */
-var CACHE = 'g04fit-v1.0.80';
+var CACHE = 'g04fit-v1.0.81';
 
 var ASSETS = [
   './',
