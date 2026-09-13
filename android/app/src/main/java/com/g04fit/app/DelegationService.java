4d3f089ad17ba40b1ff7c10dea3adfec612f91cd
PaulFV <fodorpaul@web.de>
2026-09-13 02:11:57 +0200

    Add Android TWA project for Play Store

---
+++ android/app/src/main/java/com/g04fit/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.g04fit.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
