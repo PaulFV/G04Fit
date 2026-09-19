566042ca452891c0911bdc04f53b5a148af6a8b6
PaulFV <fodorpaul@web.de>
2026-09-19 13:17:22 +0200

    Align Journey timeline dots and remove tile tint

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    eigenen Programmdateien zwischengespeichert. Der optionale
    Push-Dienst wird nur nach ausdrücklicher Einwilligung verwendet.
    ============================================================ */
-var CACHE = 'g04fit-v2.8.29';
+var CACHE = 'g04fit-v2.8.37';
 
 var ASSETS = [
   './',
