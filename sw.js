65444c3a470a758674da255ec36fcc229f9d7bc3
PaulFV <155884041+PaulFV@users.noreply.github.com>
2026-09-23 09:18:09 +0200

    Cache-Version v2.8.124
    Cache-Version v2.8.124

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    eigenen Programmdateien zwischengespeichert. Der optionale
    Push-Dienst wird nur nach ausdrücklicher Einwilligung verwendet.
    ============================================================ */
-var CACHE = 'g04fit-v2.8.123';
+var CACHE = 'g04fit-v2.8.124';
 
 var ASSETS = [
   './',
